c0c875af6842befa15eba6a186818b1e68710436
Duy Tang <duy.tang@cerebral.com>
2022-07-26 21:46:55 +0700

    Api for getting demo

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // const { ApolloServer, gql } = require('apollo-server-cloud-functions');
 const { ApolloServer, gql } = require('apollo-server');
 const { BigQuery } = require('@google-cloud/bigquery');
-
-const traffics = [
-  {date: "20210901", traffic: 553.0, dwells: 123.0, total_dwell_time: 2868.0}
-];
+const moment = require('moment');
 
 const bigqueryClient = new BigQuery();
 
@@ -17,8 +14,14 @@ const typeDefs = gql`
     total_dwell_time: Float
   }
 
+  type Demo {
+    date: String
+    demo_count: Float
+  }
+
   type Query {
     traffics(dates: [String]!, storeIds: [Int]!, productIds: [Int]!): [Traffic]
+    demos(dates: [String]!, storeIds: [Int]!, productIds: [Int]!): [Demo]
   }
 `;
 
@@ -27,32 +30,58 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     async traffics(parent, args, context, info) {
-      console.log(args)
-
       const storeIds = args['storeIds'] ? args['storeIds'].join(', ') : ''
       const productIds = args['productIds'] ? args['productIds'].join(', ') : ''
-      const dates = args['dates'] ? args['dates'].map(date => `'${date}'`).join(', ') : ''
+      const dates = args['dates'] ? args['dates'].map(date => `'${moment(date).format('YYYYMMDD')}'`).join(', ') : ''
 
-      const query = `select date, sum(traffic) traffic, sum(dwells) dwells, sum(total_dwell_time) total_dwell_time
+      if (!storeIds || !productIds || !dates) {
+        return []
+      }
+
+      const query = `SELECT date, sum(traffic) traffic, sum(dwells) dwells, sum(total_dwell_time) total_dwell_time
         FROM (
           SELECT 
           date, product_id, (traffic/ARRAY_LENGTH(product_ids)) traffic, (dwells/ARRAY_LENGTH(product_ids)) dwells, (total_dwell_time/ARRAY_LENGTH(product_ids)) total_dwell_time
-          from retail_next.placements_daily
+          FROM retail_next.placements_daily
           join UNNEST(product_ids) AS product_id
           WHERE EXISTS (SELECT *
                         FROM UNNEST(product_ids) AS x
-                        WHERE x in (${productIds}))
+                        WHERE x IN (${productIds}))
           AND date IN (${dates})
           AND store_id IN (${storeIds})
         ) t1
         WHERE t1.product_id IN (${productIds})
-        group by 1`;
-      console.log(query);
+        GROUP BY 1`;
 
       const [job] = await bigqueryClient.createQueryJob({ query: query });
-      console.log(`Job ${job.id} started.`);
       const [rows] = await job.getQueryResults();
-      console.log('Rows:', rows);
+
+      return rows
+    },
+    async demos(parent, args, context, info) {
+      const storeIds = args['storeIds'] ? args['storeIds'].join(', ') : ''
+      const productIds = args['productIds'] ? args['productIds'].join(', ') : ''
+      const dates = args['dates'] ? args['dates'].map(date => `'${moment(date).format('YYYY-MM-DD')}'`) : []
+
+      if (!storeIds || !productIds || !dates || dates.length < 1) {
+        return []
+      }
+
+      const query = `SELECT FORMAT_DATE('%Y%m%d', DATE(TIMESTAMP(timestamp), 'America/Los_Angeles')) AS date, SUM(value) AS demo_count
+        FROM metrics.displays
+        WHERE
+          product_id IN (${productIds})
+          AND store_id IN (${storeIds})
+          AND DATE(TIMESTAMP(timestamp), 'America/Los_Angeles') >= ${dates[0]}
+          AND DATE(TIMESTAMP(timestamp), 'America/Los_Angeles') <= ${dates[dates.length - 1]}
+          AND metric_name = 'demo.completed'
+        GROUP BY 1
+      `;
+
+      console.log(query)
+
+      const [job] = await bigqueryClient.createQueryJob({ query: query });
+      const [rows] = await job.getQueryResults();
 
       return rows
     },
@@ -70,5 +99,6 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
+
 
 // exports.handler = server.createHandler();
